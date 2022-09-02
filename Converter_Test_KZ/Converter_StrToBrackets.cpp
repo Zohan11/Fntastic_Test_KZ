4a5299d20032e15ab4acce0ff54eccaf19e76583
ConstantZ <aspid1108@mail.ru>
2022-09-03 01:27:14 +0300

    no message

---
+++ Converter_Test_KZ/Converter_StrToBrackets.cpp
@@ -0,0 +1,13 @@
+#include "Converter_StrToBrackets.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	Converter_StrToBrackets converter(str);
+	converter.Convert();
+}
+
+
